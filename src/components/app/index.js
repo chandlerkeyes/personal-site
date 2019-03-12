5c4c0755a43c4cf7801d9924a5ab1f8cc8ea6ce7
Chandler Keyes <chandler.keyes@interactiongaming.com>
2019-03-11 20:10:47 -0400

    Test Deploy: Displaying environment name to home page

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './styles.css';
+require('dotenv').config()
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+        <h2>Welcome</h2>
+        <p>{process.env.REACT_APP_ENVIRONMENT}</p>
       </div>
     );
   }
