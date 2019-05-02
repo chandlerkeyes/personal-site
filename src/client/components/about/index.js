9a4bf8193623a7b0a1400286ae6822b752f6d307
chandlerkeyes <chandler.keyes@interactiongaming.com>
2019-05-01 20:21:55 -0400

    Started front end work for about page

--- src/client/components/about/index.js
+++ src/client/components/about/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { ASSETS } from "../../assets/";
 import './styles.scss';
 
 class About extends React.Component {
   render() {
     return (
       <div className="about">
-      About Page
+        <div className="about-me-container">
+          <img className="header-image" src={ASSETS.ABOUT_ME} alt="About Me Image" />
+          <h3 className="header-title">About Me</h3>
+          <div className="about-me-navigation">
+            <button>Summary</button>
+            <button>Work Experience</button>
+            <button>Education</button>
+          </div>
+          <hr/>
+        </div>
       </div>
     );
   }
